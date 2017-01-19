6f4f1e37d8220141c0cacf0b597ae72937ea66d2
Nicole Betances <nicole09424@gmail.com>
2017-01-19 21:38:31 +0000

    save

---
+++ script.js
@@ -0,0 +1,17 @@
+TinyTurtle.apply(window); 
+
+forward(40);
+right(90);
+forward(60);
+right(90);
+forward(40);
+right(90);
+forward(60);
+right(90);
+forward(40);
+right(45);
+forward(40);
+right(90);
+forward(40);
+
+stamp()
